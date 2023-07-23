963a4b160f34a24cb2732dab0fed5b8f9799bccb
dquangv <dquangvu7998@gmail.com>
2023-07-23 13:36:00 +0700

    Upload lab 6

--- lab/lab6/file.js
+++ lab/lab6/file.js
@@ -35,6 +35,10 @@ function lady() {
 }
 
 
+
+
+
+
 //bai 2 cau b
 let imgEle = document.querySelector('img');
 
@@ -45,6 +49,10 @@ imgEle.addEventListener('mouseover', function() {
 imgEle.addEventListener('mouseout', function() {
     imgEle.src = '2.jpg';
 });
+
+
+
+
 
 
 //bai 2 cau c
@@ -66,7 +74,78 @@ btnChild[2].addEventListener('click', function() {
 
     buttonImg = document.createElement('img');
     buttonImg.src = '2.jpg';
+    buttonImg.style = 'width: 200px; height: 200px;';
     
     btnChild[3].insertAdjacentElement('afterend',buttonText2);
     btnChild[3].nextElementSibling.insertAdjacentElement('afterend',buttonImg);
+});
+
+btnChild[3].addEventListener('click', function() {
+    if (buttonText2) {
+        if (buttonText2.textContent === 'I am a lady') {
+            return;
+        } else {
+            buttonText2.remove();
+            buttonImg.remove();
+        }
+    }
+
+    buttonText2 = document.createElement('p');
+    buttonText2.style.color = 'pink';
+    buttonText2.textContent = 'I am a lady';
+
+    buttonImg = document.createElement('img');
+    buttonImg.src = '3.jpg';
+    buttonImg.style = 'width: 200px; height: 200px;';
+
+    btnChild[3].insertAdjacentElement('afterend',buttonText2);
+    btnChild[3].nextElementSibling.insertAdjacentElement('afterend',buttonImg);
+});
+
+
+
+
+
+
+//bai 2 cau d
+document.addEventListener('keydown', function(event) {
+    alert(`You press ${event.key}`);
+})
+
+
+
+
+
+//bai 2 cau e
+document.querySelector('input[name="username"]').addEventListener('click', function() {
+    alert(`You are inputing ${this.name}`);
+});
+
+document.querySelector('input[name="password"]').addEventListener('click', function() {
+    alert(`You are inputing ${this.name}`);
+});
+
+
+
+
+
+//bai 3
+function randomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    
+    for (let i = 0; i <6; i++) {
+        color += letters[Math.floor(Math.random()*16)];
+    }
+
+    return color;
+}
+
+btnChild[4].addEventListener('click', function() {
+    document.querySelector('.box').style.backgroundColor = randomColor();
+});
+
+btnChild[5].addEventListener('click', function() {
+    let curPosition = parseInt(document.querySelector('.box').style.left || 0);
+    document.querySelector('.box').style.left = (curPosition + 10) + 'px';
 });
