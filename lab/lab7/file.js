e749475150e2158be4ce2464d537317473c45300
dquangv <dquangvu7998@gmail.com>
2023-07-26 00:16:04 +0700

    Update lab7.1

--- lab/lab7/file.js
+++ lab/lab7/file.js
@@ -1,33 +1,76 @@
-let opeButtons = document.querySelectorAll(".operator");
+let numButtons = document.querySelectorAll(".number");
+let opeButtons = document.querySelectorAll(".operation");
 let calButton = document.querySelector(".calculate");
 let clrButton = document.querySelector(".clear");
 let display = document.querySelector("#display");
 let resultDisplay = document.querySelector("#result");
 
-function appendDisplay(value) {
+let firstNumber = null;
+let operator = null;
+let flagClear = false;
+let flagCal = false;
+
+numButtons.forEach(button => {
+    button.addEventListener("click", function () {
+        appendNumDisplay(button.textContent);
+    });
+});
+
+opeButtons.forEach(button => {
+    button.addEventListener("click", function () {
+        operation(button.textContent);
+    });
+});
+
+function appendNumDisplay(value) {
+    if (flagClear) {
+        display.value = "";
+        flagClear = false;
+    }
     display.value += value;
 }
 
-function calculate() {
-    let result = null;
-    
-    try {
-        result = eval(display.value);
-    } catch (e) {
-        result = 'Error';
+function operation(value) {
+    if (flagCal && value !== '=') {
+        calculate(value);
     }
 
-    resultDisplay.textContent = result;
+    firstNumber = parseFloat(display.value);
+    operator = value;
+    flagClear = true;
+    flagCal = true;
+}
+
+function calculate(value) {
+    flagCal = false;
+    let secondNumber = parseFloat(display.value);
+    switch (operator) {
+        case "+":
+            result = firstNumber + secondNumber;
+            break;
+        case "-":
+            result = firstNumber - secondNumber;
+            break;
+        case "x":
+            result = firstNumber * secondNumber;
+            break;
+        case ":":
+            result = firstNumber / secondNumber;
+            break;
+        default:
+            result = null;
+    }
+
+    display.value = result;
 }
 
 function clear() {
-    display.value = '';
-    resultDisplay.textContent = '';
+    display.value = "";
+    resultDisplay.textContent = "";
+    firstNumber = null;
+    operator = null;
+    countCal = 0;
 }
-
-opeButtons.forEach(button => {
-    button.addEventListener("click", () => appendDisplay(button.textContent));
-});
 
 calButton.addEventListener("click", calculate);
 
