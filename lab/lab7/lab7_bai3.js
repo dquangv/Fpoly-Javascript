fdec3eb04bed53172e1c81fda07c01b280fb50d8
dquangv <dquangvu7998@gmail.com>
2023-07-27 14:34:41 +0700

    Update lab 7.3

--- lab/lab7/lab7_bai3.js
+++ lab/lab7/lab7_bai3.js
@@ -1,60 +1,39 @@
-window.addEventListener('load', function() {
+//khi web load xong
+window.addEventListener('load', function () {
     let colorBox = document.querySelector("#colorBox");
     let messageBox = document.querySelector("#messageBox");
     let container = document.querySelector("#container");
-    let startTime = null;
-    let endTime = null;
+    //tạo biến lưu trữ đối tượng thời gian tại thời điểm mới load xong web
+    let startTime = new Date();
 
+    //hàm lấy vị trí bất kì trong khung hình vuông
     function randomPosition() {
         let heightBox = colorBox.offsetHeight;
         let widthBox = colorBox.offsetWidth;
         let heightContainer = container.offsetHeight;
         let widthContainer = container.offsetWidth;
 
-        let posX = Math.floor(Math.random() * (heightContainer - heightBox));
-        let posY = Math.floor(Math.random() * (widthContainer - widthBox));
+        //lấy kích thước khung trừ kích thước hộp để đảm bảo hộp nằm trong khung, nhân với random trong khoảng (0,1) và làm tròn xuống để cho ra toạ độ bất kì từ 0 đến toạ độ tối đa để hộp nằm trong khung
+        let posX = Math.floor(Math.random() * (widthContainer - widthBox));
+        let posY = Math.floor(Math.random() * (heightContainer - heightBox));
 
-        return {x: posX, y: posY};
+        return { x: posX, y: posY };
     }
 
-    colorBox.addEventListener('click', function() {
-        let randomPos = randomPosition();
-        colorBox
+    let randomPos = randomPosition();
+
+    //gán toạ độ bất kì vào cho hộp
+    colorBox.style.left = `${randomPos.x}px`;
+    colorBox.style.top = `${randomPos.y}px`;
+
+    //khi hộp được click
+    colorBox.addEventListener('click', function () {
+        //tạo đối tượng thời gian lưu trữ thời điểm click hộp
+        let endTime = new Date();
+        
+        colorBox.style.display = 'none';
+
+        //khoảng thời gian = thời điểm click hộp trừ thời điểm load web, chia 1000 để đổi từ mili giây sang giây
+        messageBox.textContent = `It took ${(endTime - startTime) / 1000} seconds to click`;
     });
 });
-
-//     let startTime;
-//     let timer;
-
-//     function showColorBox() {
-//         const randomPos = getRandomPosition();
-//         colorBox.style.left = `${randomPos.x}px`;
-//         colorBox.style.top = `${randomPos.y}px`;
-//         colorBox.style.display = "block";
-//         startTime = new Date();
-//         timer = setInterval(() => {
-//             const currentTime = new Date();
-//             const elapsedTime = (currentTime - startTime) / 1000;
-//             messageBox.textContent = `It took ${elapsedTime.toFixed(2)} seconds to click`;
-//         }, 10);
-//     }
-
-//     colorBox.addEventListener("click", function () {
-//         clearInterval(timer);
-//         colorBox.style.display = "none";
-//         messageBox.style.display = "block";
-//     });
-
-//     container.addEventListener("click", function (event) {
-//         if (event.target.id === "container") {
-//             showColorBox();
-//             messageBox.style.display = "none";
-//         }
-//     });
-
-//     showColorBox();
-// });
-
-
-let heightBox = colorBox.offsetHeight;
-console.log(heightBox);
